541a298df303a4e485a23705b8daff1266e48282
MichalTurski <turski.michal.antoni@gmail.com>
2019-05-15 12:10:05 +0200

    Add dataloader.

--- DataLoading.py
+++ DataLoading.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import torch.utils.data
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
 class SpectrogramDataset(torch.utils.data.Dataset):
@@ -15,9 +16,32 @@ class SpectrogramDataset(torch.utils.data.Dataset):
         return len(self.files)
 
     def __getitem__(self, idx):
-        spect_name = os.path.join(self.path, self.files.iloc[idx, 0])
+        spect_name = os.path.join(self.path, self.files[idx])
         spectrogram = np.load(spect_name)
-        cls = self.classes.iloc[idx, 0]
+        cls = self.classes.iloc[idx]
         sample = {'spectrogram': spectrogram, 'cls': cls}
 
         return sample
+
+
+def load_data(batch_size, num_workers, validation_split, shuffle, seed, path, filename):
+    spect_dataset = SpectrogramDataset(path, filename)
+
+    # Creating data indices for training and validation splits:
+    dataset_size = len(spect_dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle:
+        np.random.seed(seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = torch.utils.data.DataLoader(spect_dataset, batch_size=batch_size,
+                                               sampler=train_sampler, num_workers=num_workers)
+    validation_loader = torch.utils.data.DataLoader(spect_dataset, batch_size=batch_size,
+                                                    sampler=valid_sampler, num_workers=num_workers)
+    return train_loader, validation_loader
--- PreprocessAudio.py
+++ PreprocessAudio.py
@@ -2,7 +2,6 @@ import os
 import librosa
 import librosa.display
 import numpy as np
-import pickle
 
 def create_spectrogram(path):
     y, sr = librosa.load(path)
@@ -19,37 +18,38 @@ def plot_spectrogram(spectrogram):
     plt.show()
 
 
-root_dir = './audio_dataset'
-out_dir = './spectrogram_dataset'
-description_file = 'All_files.csv'
+def run():
+    root_dir = './audio_dataset'
+    out_dir = './spectrogram_dataset'
+    description_file = 'All_files.csv'
+    create_files_switch = False
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-desc_file = open(os.path.join(out_dir, description_file), "w")
-for (top_dir, subdirectories, _) in os.walk(root_dir):
-    for idx1, sub_dir in enumerate(subdirectories):
-        print(f"I'm processing \"{sub_dir}\".")
-        for (directory, _, files) in os.walk(os.path.join(top_dir, sub_dir)):
-            for idx2, file in enumerate(files):
-                # if idx1 == idx2:
-                in_path = os.path.join(directory, file)
-                (filename, _) = os.path.splitext(file)
-                filename_extended = sub_dir + "_" + filename + ".npy"
-                out_path = os.path.join(out_dir, filename_extended)
-                # if idx1 == idx2 == 0:
-                #     print(in_path)
-                #     print(out_path)
+    desc_file = open(os.path.join(out_dir, description_file), "w")
+    for (top_dir, subdirectories, _) in os.walk(root_dir):
+        for idx1, sub_dir in enumerate(subdirectories):
+            print(f"I'm processing \"{sub_dir}\".")
+            for (directory, _, files) in os.walk(os.path.join(top_dir, sub_dir)):
+                for idx2, file in enumerate(files):
+                    # if idx1 == idx2:
+                    in_path = os.path.join(directory, file)
+                    (filename, _) = os.path.splitext(file)
+                    filename_extended = sub_dir + "_" + filename + ".npy"
+                    if create_files_switch:
+                        out_path = os.path.join(out_dir, filename_extended)
 
-                spectrogram = create_spectrogram(in_path)
-                # plot_spectrogram(spectrogram)
-                # print(spectrogram.shape)
+                        spectrogram = create_spectrogram(in_path)
+                        # plot_spectrogram(spectrogram)
+                        # print(spectrogram.shape)
 
-                pickle_file = open(out_path, "wb")
-                # pickle.dump(spectrogram, pickle_file)
-                np.save(pickle_file, spectrogram)
-                pickle_file.close()
+                        pickle_file = open(out_path, "wb")
+                        np.save(pickle_file, spectrogram)
+                        pickle_file.close()
 
+                    desc_file.write(f"{filename_extended}, {sub_dir}\n")
+    desc_file.close()
 
-                desc_file.write(f"{filename_extended}, {sub_dir}\n")
-desc_file.close()
+#run()
+
