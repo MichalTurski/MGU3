513ab09a48fbde11d87fc3bf918cafa73d077c9f
MichalTurski <turski.michal.antoni@gmail.com>
2019-05-15 10:49:37 +0200

    Initial commit

---
+++ PreprocessAudio.py
@@ -0,0 +1,55 @@
+import os
+import librosa
+import librosa.display
+import numpy as np
+import pickle
+
+def create_spectrogram(path):
+    y, sr = librosa.load(path)
+    return librosa.feature.melspectrogram(y=y, sr=sr)
+
+
+def plot_spectrogram(spectrogram):
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(librosa.power_to_db(spectrogram, ref = np.max), y_axis = 'mel', fmax = 8000, x_axis = 'time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel spectrogram')
+    plt.tight_layout()
+    plt.show()
+
+
+root_dir = './audio_dataset'
+out_dir = './spectrogram_dataset'
+description_file = 'All_files.csv'
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+desc_file = open(os.path.join(out_dir, description_file), "w")
+for (top_dir, subdirectories, _) in os.walk(root_dir):
+    for idx1, sub_dir in enumerate(subdirectories):
+        print(f"I'm processing \"{sub_dir}\".")
+        for (directory, _, files) in os.walk(os.path.join(top_dir, sub_dir)):
+            for idx2, file in enumerate(files):
+                # if idx1 == idx2:
+                in_path = os.path.join(directory, file)
+                (filename, _) = os.path.splitext(file)
+                filename_extended = sub_dir + "_" + filename + ".npy"
+                out_path = os.path.join(out_dir, filename_extended)
+                # if idx1 == idx2 == 0:
+                #     print(in_path)
+                #     print(out_path)
+
+                spectrogram = create_spectrogram(in_path)
+                # plot_spectrogram(spectrogram)
+                # print(spectrogram.shape)
+
+                pickle_file = open(out_path, "wb")
+                # pickle.dump(spectrogram, pickle_file)
+                np.save(pickle_file, spectrogram)
+                pickle_file.close()
+
+
+                desc_file.write(f"{filename_extended}, {sub_dir}\n")
+desc_file.close()
